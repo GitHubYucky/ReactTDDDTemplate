e10984add40142cf7e1e8375d5841ed92f08588c
GitHubYucky <registyucky@gmail.com>
2025-08-09 22:19:55 +0900

    API受付用の型を用意

--- src/features/coffee/type/coffee.ts
+++ src/features/coffee/type/coffee.ts
@@ -1,7 +1,14 @@
-export type Coffee = {
+export type CoffeeType = {
     title: string;
     description: string;
     ingredients: string[];
+    image: string;
+    id: number;
+  };
+  export type CoffeeApiType = {
+    title: string;
+    description: string;
+    ingredients: string[]|string;
     image: string;
     id: number;
   };

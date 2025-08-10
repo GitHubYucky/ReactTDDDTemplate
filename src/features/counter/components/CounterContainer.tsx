dea3abf74bd4a1bb97ff93351379186f13d72ba1
YukioAso <registyucky@gmail.com>
2025-08-10 21:01:37 +0900

    fix for counter controls

--- src/features/counter/components/CounterContainer.tsx
+++ src/features/counter/components/CounterContainer.tsx
@@ -1,18 +1,22 @@
 import { useCount } from "../hooks/useCount";
 import { CountDisplay } from "./CountDisplay";
-import { Button } from "../../../components/button/button";
+import { CountControls } from "./CountControls";
+import styles from "./CounterContainer.module.css";
 
-export const CounterContainer=()=>{
-    const {count,countUp,countDown,countUpFive,countDownFive,reset}=useCount();
+export const CounterContainer = () => {
+  const { count, countUp, countDown, countUpFive, countDownFive, reset } =
+    useCount();
 
-    return (
-        <div>
-            <CountDisplay count={count}/>
-            <Button onClick={countUp}>CountUp</Button>
-            <Button onClick={countDown}>CountDown</Button>
-            <Button onClick={countUpFive}>CountUp5</Button>
-            <Button onClick={countDownFive}>CountDown5</Button>
-            <Button onClick={reset}>Reset</Button>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <CountDisplay count={count} />
+      <CountControls
+        onCountUp={countUp}
+        onCountDown={countDown}
+        onCountUpFive={countUpFive}
+        onCountDownFive={countDownFive}
+        onReset={reset}
+      />
+    </div>
+  );
+};

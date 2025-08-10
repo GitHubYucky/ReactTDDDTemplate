6d84ac0e675e064177b4f7bcb8f295cf813e5ec9
YukioAso <registyucky@gmail.com>
2025-08-10 14:42:25 +0900

    ddddd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactTDDDTemplate/', // ← リポジトリ名に合わせる
+  base: process.env.VITE_BASE || '/', // ← Actions/ローカル両対応
   server: {
     proxy: {
       "/api": "http://localhost:3001",

a8961cab7d478cff9aa407238baffc841657fc84
YukioAso <registyucky@gmail.com>
2025-08-10 14:59:17 +0900

    ccc

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@
 import { defineConfig } from 'vite'; // ← ここを修正！
 import react from '@vitejs/plugin-react';
 
+// vite.config.ts
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE || '/', // ← Actions/ローカル両対応
   server: {
-    proxy: {
-      "/api": "http://localhost:3001",
-    },
-  }
+    proxy: { "/api": { target: "http://localhost:3000", changeOrigin: true } }
+  },
+  base: process.env.VITE_BASE || "/ReactTDDDTemplate/",
 });

94d8b1aaa474d1fe9b327f383fd7cd0c15d65a4c
YukioAso <registyucky@gmail.com>
2025-08-15 02:05:19 +0900

    handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { echoHandler } from "../features/echo/apis/echo";
+import { mp3Handler } from "../features/mp3-downloader/apis/mp3"
+
+
 const app = express();
 app.use(bodyParser.json());
 
 app.all("/api/echo", echoHandler);
+app.all("/api/mp3", mp3Handler);
 
 const PORT = Number(process.env.PORT) || 3001;
 app.listen(PORT, () => {

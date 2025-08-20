18c364151a01dd02c975a0f4af635af85cf062a8
YukioAso <registyucky@gmail.com>
2025-08-21 03:16:26 +0900

    prepare for tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import {App} from '@/App'
 import { Provider } from 'react-redux'
 import { store } from './stores/store'
+import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
